21dd414b1779e5e462e4b7bcb68c78f0f6835864
Ron Lisle <ron@lisles.net>
2014-10-08 08:17:15 -0500

    Starting Beacon object

---
+++ Beacon/Beacon/Beacon.swift
@@ -0,0 +1,16 @@
+//
+//  Beacon.swift
+//  Beacon
+//
+//  This is the Beacon model file.
+//  It can be paired with a beacon, and then will report the distance to it.
+//
+//  Created by Ron Lisle on 10/8/14.
+//  Copyright (c) 2014 Ron Lisle. All rights reserved.
+//
+
+import Foundation
+
+class Beacon {
+    
+}
